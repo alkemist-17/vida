6fc388dc0bb2e2c0858df7fe83c57951a27ae478
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2025-07-16 11:40:58 -0500

    Add protected call to std exception

--- foundation.go
+++ foundation.go
@@ -8,15 +8,33 @@ import (
 
 func loadFoundationException() Value {
 	m := &Object{Value: make(map[string]Value)}
-	m.Value["raise"] = GFn(riseException)
+	m.Value["raise"] = GFn(raiseException)
+	m.Value["protected"] = GFn(catchException)
 	return m
 }
 
-func riseException(args ...Value) (Value, error) {
+func raiseException(args ...Value) (Value, error) {
 	if len(args) > 0 {
 		err := fmt.Errorf("%s", fmt.Sprintf("\n\n  [%v]\n   Message : %v\n\n", verror.ExceptionErrType, args[0].String()))
 		return NilValue, err
 	}
 	err := fmt.Errorf("%s", fmt.Sprintf("\n\n  [%v]\n\n", verror.ExceptionErrType))
 	return NilValue, err
+}
+
+func catchException(args ...Value) (Value, error) {
+	if len(args) > 0 {
+		if fn, ok := args[0].(*Function); ok {
+			vm := &VM{newThread(fn, ((*clbu)[globalStateIndex].(*GlobalState)).Script, picoStack)}
+			if _, err := vm.runThread(0, 0, true, args[1:]...); err == nil {
+				return vm.Channel, nil
+			} else {
+				switch e := err.(type) {
+				case verror.VidaError:
+					return &Error{Message: &String{Value: e.Message}}, nil
+				}
+			}
+		}
+	}
+	return NilValue, nil
 }
