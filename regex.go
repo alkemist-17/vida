d1040ab20a222cdda73c63b3db175a6428677596
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-03-21 15:40:13 -0500

    add findIndex to regex lib

--- regex.go
+++ regex.go
@@ -6,6 +6,7 @@ func loadFoundationRegexp() Value {
 	m := &Object{Value: make(map[string]Value)}
 	m.Value["match"] = GFn(regexpMatch)
 	m.Value["replaceAll"] = GFn(regexpReplaceAll)
+	m.Value["findIndex"] = GFn(regexpFindFirstIndex)
 	return m
 }
 
@@ -35,6 +36,28 @@ func regexpReplaceAll(args ...Value) (Value, error) {
 				return &Error{Message: &String{Value: err.Error()}}, nil
 			}
 			return &String{Value: re.ReplaceAllString(source.Value, replacement.Value)}, nil
+		}
+	}
+	return NilValue, nil
+}
+
+func regexpFindFirstIndex(args ...Value) (Value, error) {
+	if len(args) > 1 {
+		pattern, okPatt := args[0].(*String)
+		input, okIn := args[1].(*String)
+		if okPatt && okIn {
+			re, err := regexp.Compile(pattern.Value)
+			if err != nil {
+				return &Error{Message: &String{Value: err.Error()}}, nil
+			}
+			res := re.FindStringIndex(input.Value)
+			if res == nil {
+				return NilValue, nil
+			}
+			arr := &Array{Value: make([]Value, 2)}
+			arr.Value[0] = Integer(res[0])
+			arr.Value[1] = Integer(res[1])
+			return arr, nil
 		}
 	}
 	return NilValue, nil
