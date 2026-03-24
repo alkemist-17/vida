966d57e4ef076f565345e33f2d4a8c382553f541
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-03-23 21:11:59 -0500

    add escape and other funcs to re lib

--- regex.go
+++ regex.go
@@ -6,7 +6,10 @@ func loadFoundationRegexp() Value {
 	m := &Object{Value: make(map[string]Value)}
 	m.Value["match"] = GFn(regexpMatch)
 	m.Value["replaceAll"] = GFn(regexpReplaceAll)
-	m.Value["findIndex"] = GFn(regexpFindFirstIndex)
+	m.Value["findString"] = GFn(regexpFindString)
+	m.Value["findAllStrings"] = GFn(regexpFindAllStrings)
+	m.Value["findFirstIndex"] = GFn(regexpFindFirstIndex)
+	m.Value["escape"] = GFn(regexpEscape)
 	return m
 }
 
@@ -57,6 +60,51 @@ func regexpFindFirstIndex(args ...Value) (Value, error) {
 			arr := &Array{Value: make([]Value, 2)}
 			arr.Value[0] = Integer(res[0])
 			arr.Value[1] = Integer(res[1])
+			return arr, nil
+		}
+	}
+	return NilValue, nil
+}
+
+func regexpEscape(args ...Value) (Value, error) {
+	if len(args) > 0 {
+		if input, ok := args[0].(*String); ok {
+			return &String{Value: regexp.QuoteMeta(input.Value)}, nil
+		}
+	}
+	return NilValue, nil
+}
+
+func regexpFindString(args ...Value) (Value, error) {
+	if len(args) > 1 {
+		pattern, okPatt := args[0].(*String)
+		input, okIn := args[1].(*String)
+		if okPatt && okIn {
+			re, err := regexp.Compile(pattern.Value)
+			if err != nil {
+				return &Error{Message: &String{Value: err.Error()}}, nil
+			}
+			return &String{Value: re.FindString(input.Value)}, nil
+		}
+	}
+	return NilValue, nil
+}
+
+func regexpFindAllStrings(args ...Value) (Value, error) {
+	if len(args) > 2 {
+		pattern, okPatt := args[0].(*String)
+		input, okIn := args[1].(*String)
+		n, okn := args[2].(Integer)
+		if okPatt && okIn && okn {
+			re, err := regexp.Compile(pattern.Value)
+			if err != nil {
+				return &Error{Message: &String{Value: err.Error()}}, nil
+			}
+			result := re.FindAllString(input.Value, int(n))
+			arr := &Array{Value: make([]Value, len(result))}
+			for i, v := range result {
+				arr.Value[i] = &String{Value: v}
+			}
 			return arr, nil
 		}
 	}
