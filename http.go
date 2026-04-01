89ce1383516915c550bc6890bd2246f7b6e3e7d2
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-04-01 11:16:59 -0500

    http client poc

--- http.go
+++ http.go
@@ -1,9 +1,6 @@
 package vida
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -12,195 +9,171 @@ import (
 	"time"
 )
 
-var defaultClient *HttpClient
-var validMethods map[string]bool
-
-type HttpClient struct {
-	BaseURL    string
-	HTTPClient *http.Client
-}
+const (
+	httpInvalidURLErr = "invalid url"
+	httpNetworkErr    = "network"
+	httpTimeoutErr    = "timeout"
+	httpBodyReadErr   = "body read"
+	httpLargeBodyErr  = "large body"
+	httpMaxBodySize   = 10 << 20
+)
 
 func loadFoundationHttpClient() Value {
-	defaultClient = &HttpClient{HTTPClient: &http.Client{}}
-	validMethods = map[string]bool{
-		"GET":     true,
-		"POST":    true,
-		"PUT":     true,
-		"DELETE":  true,
-		"PATCH":   true,
-		"HEAD":    true,
-		"OPTIONS": true,
-	}
 	m := &Object{Value: make(map[string]Value)}
 	m.Value["request"] = GFn(httpRequest)
+	m.Value["get"] = GFn(httpRequest)
+	m.Value["header"] = GFn(responseHeaderHelper)
+	m.Value["text"] = GFn(responseBodyToString)
 	return m
 }
 
-func httpRequest(args ...Value) (Value, error) {
-	l := len(args)
-	if l == 1 {
-		if url, ok := args[0].(*String); ok {
-			options := httpCreateDefaultRequestOptions(url)
-			response, err := defaultClient.RequestWithDefaultOptions(options)
-			if err != nil {
-				return VidaError{Message: &String{Value: err.Error()}}, nil
+type Response struct {
+	StatusCode int
+	Headers    map[string]string
+	Body       []byte
+	URL        string
+	Elapsed    time.Duration
+}
+
+func request(rawURL string) (*http.Response, []byte, error) {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return nil, nil, &RequestError{
+			Kind:    httpInvalidURLErr,
+			Message: fmt.Sprintf("failed to parse URL %q", rawURL),
+			Err:     err,
+		}
+	}
+
+	if parsedURL.Scheme == "" {
+		parsedURL.Scheme = "https"
+		rawURL = parsedURL.String()
+	}
+
+	resp, err := http.Get(rawURL)
+
+	if err != nil {
+		kind := httpNetworkErr
+		if urlErr, ok := err.(*url.Error); ok {
+			if urlErr.Timeout() {
+				kind = httpTimeoutErr
 			}
-			return response, nil
+		}
+		return nil, nil, &RequestError{
+			Kind:    kind,
+			Message: fmt.Sprintf("request to %q failed", rawURL),
+			Err:     err,
+		}
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(io.LimitReader(resp.Body, httpMaxBodySize+1))
+
+	if err != nil {
+		return nil, nil, &RequestError{
+			Kind:    httpBodyReadErr,
+			Message: "failed to read response body",
+			Err:     err,
+		}
+	}
+
+	if int64(len(body)) > httpMaxBodySize {
+		return nil, nil, &RequestError{
+			Kind:    httpLargeBodyErr,
+			Message: fmt.Sprintf("response body exceeds %d bytes", httpMaxBodySize),
+		}
+	}
+
+	headers := make(map[string]string, len(resp.Header))
+
+	for key, values := range resp.Header {
+		if len(values) > 0 {
+			headers[key] = values[0]
+		}
+	}
+
+	return resp, body, nil
+}
+
+func httpRequest(args ...Value) (Value, error) {
+	if len(args) == 1 {
+		urlVal, ok := args[0].(*String)
+		if !ok {
+			return VidaError{Message: &String{Value: fmt.Sprintf("http.request: first argument must be string, got %T", args[0])}}, nil
+		}
+		resp, body, err := request(urlVal.Value)
+		if err != nil {
+			if reqErr, ok := err.(*RequestError); ok {
+				errObj := &Object{
+					Value: map[string]Value{
+						"kind":    &String{Value: reqErr.Kind},
+						"message": &String{Value: reqErr.Message},
+						"url":     urlVal,
+					},
+				}
+				return VidaError{Message: errObj}, nil
+			}
+			return VidaError{Message: &String{Value: err.Error()}}, nil
+		}
+		return responseToObject(resp, body), nil
+	}
+	return NilValue, nil
+}
+
+func responseToObject(resp *http.Response, body []byte) *Object {
+	headersObj := &Object{Value: make(map[string]Value)}
+	for k, v := range resp.Header {
+		if len(v) > 0 {
+			headersObj.Value[k] = &String{Value: v[0]}
+		}
+	}
+	dl, _ := resp.Request.Context().Deadline()
+	return &Object{
+		Value: map[string]Value{
+			"statusCode": Integer(resp.StatusCode),
+			"headers":    headersObj,
+			"body":       &Bytes{Value: body},
+			"url":        &String{Value: resp.Request.URL.String()},
+			"elapsed":    Integer(time.Since(dl).Milliseconds()),
+		},
+	}
+}
+
+func responseBodyToString(args ...Value) (Value, error) {
+	if len(args) > 0 {
+		if respObj, ok := args[0].(*Object); ok {
+			if body, ok := respObj.Value["body"].(*Bytes); ok {
+				return &String{Value: string(body.Value)}, nil
+			}
 		}
 	}
 	return NilValue, nil
 }
 
-func httpCreateResponseObject(statusCode Integer, headers *Object, body []byte) *Object {
-	response := &Object{Value: make(map[string]Value)}
-	response.Value["statusCode"] = statusCode
-	response.Value["headers"] = headers
-	response.Value["body"] = &Bytes{Value: body}
-	return response
-}
-
-func httpCreateDefaultRequestOptions(url *String) *Object {
-	reqOps := &Object{Value: make(map[string]Value)}
-	reqOps.Value["method"] = &String{Value: "GET"}
-	reqOps.Value["url"] = url
-	reqOps.Value["baseURL"] = &String{Value: ""}
-	reqOps.Value["params"] = &Object{Value: make(map[string]Value)}
-	reqOps.Value["body"] = &Object{Value: make(map[string]Value)}
-	reqOps.Value["headers"] = &Object{Value: make(map[string]Value)}
-	reqOps.Value["timeout"] = Integer(5000)
-	reqOps.Value["auth"] = &Object{}
-	reqOps.Value["responseType"] = &String{Value: "json"}
-	reqOps.Value["responseEncoding"] = &String{Value: "utf8"}
-	reqOps.Value["maxRedirects"] = Integer(21)
-	reqOps.Value["maxContentLength"] = Integer(192 * 1024)
-	reqOps.Value["maxBodyLength"] = Integer(192 * 1024)
-	reqOps.Value["decompress"] = Bool(true)
-	return reqOps
-}
-
-func (c *HttpClient) RequestWithDefaultOptions(options *Object) (Value, error) {
-	upperMethod := strings.ToUpper(options.Value["method"].(*String).Value)
-	if !validMethods[upperMethod] {
-		return VidaError{Message: &String{Value: fmt.Errorf("invalid HTTP method: %q", options.Value["method"]).Error()}}, nil
-	}
-	var fullURL string
-	if c.BaseURL != "" {
-		var err error
-		fullURL, err = url.JoinPath(c.BaseURL, options.Value["url"].(*String).Value)
-		if err != nil {
-			return VidaError{Message: &String{Value: err.Error()}}, nil
-		}
-	} else if options.Value["baseURL"].(*String).Value != "" {
-		var err error
-		fullURL, err = url.JoinPath(options.Value["baseURL"].(*String).Value, options.Value["url"].(*String).Value)
-		if err != nil {
-			return VidaError{Message: &String{Value: err.Error()}}, nil
-		}
-	} else {
-		fullURL = options.Value["url"].(*String).Value
-	}
-
-	if len(options.Value["params"].(*Object).Value) > 0 {
-		parsedURL, err := url.Parse(fullURL)
-		if err != nil {
-			return VidaError{Message: &String{Value: err.Error()}}, nil
-		}
-		q := parsedURL.Query()
-		for k, v := range options.Value["params"].(*Object).Value {
-			if paramValue, ok := v.(*String); ok {
-				q.Add(k, paramValue.Value)
+func responseHeaderHelper(args ...Value) (Value, error) {
+	if len(args) > 1 {
+		respObj, okresp := args[0].(*Object)
+		name, okname := args[1].(*String)
+		if okresp && okname {
+			if headers, ok := respObj.Value["headers"].(*Object); ok {
+				for k, v := range headers.Value {
+					if strings.EqualFold(k, name.Value) {
+						return v, nil
+					}
+				}
 			}
 		}
-		parsedURL.RawQuery = q.Encode()
-		fullURL = parsedURL.String()
 	}
-
-	var bodyReader io.Reader
-	var bodyLength int64
-
-	switch v := options.Value["body"].(type) {
-	case *String:
-		bodyReader = strings.NewReader(v.Value)
-		bodyLength = int64(len(v.Value))
-	case *Bytes:
-		bodyReader = bytes.NewReader(v.Value)
-		bodyLength = int64(len(v.Value))
-	default:
-		jsonBody, err := json.Marshal(options.Value["body"])
-		if err != nil {
-			return VidaError{Message: &String{Value: err.Error()}}, nil
-		}
-		bodyReader = bytes.NewBuffer(jsonBody)
-		bodyLength = int64(len(jsonBody))
-	}
-	if options.Value["maxBodyLength"].(Integer) > 0 && Integer(bodyLength) > options.Value["maxBodyLength"].(Integer) {
-		return VidaError{Message: &String{Value: errors.New("request body length exceeded maxBodyLength").Error()}}, nil
-	}
-
-	req, err := http.NewRequest(options.Value["method"].(*String).Value, fullURL, bodyReader)
-
-	if err != nil {
-		return VidaError{Message: &String{Value: err.Error()}}, nil
-	}
-
-	headers, _ := options.Value["headers"].(*Object)
-	if _, exists := headers.Value["Content-Type"]; !exists {
-		headers.Value["Content-Type"] = &String{Value: "application/json"}
-	}
-
-	for key, v := range headers.Value {
-		if str, ok := v.(*String); ok {
-			req.Header.Set(key, str.Value)
-		}
-	}
-
-	httpClient := &http.Client{
-		Timeout: time.Duration(options.Value["timeout"].(Integer)) * time.Millisecond,
-	}
-
-	if options.Value["maxRedirects"].(Integer) > 0 {
-		httpClient.CheckRedirect = func(_ *http.Request, via []*http.Request) error {
-			if len(via) >= int(options.Value["maxRedirects"].(Integer)) {
-				return fmt.Errorf("too many redirects (max: %d)", options.Value["maxRedirects"].(Integer))
-			}
-			return nil
-		}
-	}
-
-	resp, err := httpClient.Do(req)
-
-	if err != nil {
-		return VidaError{Message: &String{Value: err.Error()}}, nil
-	}
-
-	defer func() {
-		if cerr := resp.Body.Close(); cerr != nil {
-			if err != nil {
-				err = fmt.Errorf("%w; failed to close response body: %v", err, cerr)
-			} else {
-				err = fmt.Errorf("failed to close response body: %v", cerr)
-			}
-		}
-	}()
-
-	var responseBody []byte
-	limitedReader := io.LimitReader(resp.Body, int64(options.Value["maxContentLength"].(Integer))+1)
-	responseBody, err = io.ReadAll(limitedReader)
-	if err != nil {
-		return VidaError{Message: &String{Value: err.Error()}}, nil
-	}
-
-	if Integer(len(responseBody)) > options.Value["maxContentLength"].(Integer) {
-		return VidaError{Message: &String{Value: errors.New("response content length exceeded maxContentLength").Error()}}, nil
-	}
-
-	return httpCreateResponseObject(Integer(resp.StatusCode), httpCreateObjectFromHeader(resp.Header), responseBody), err
+	return NilValue, nil
 }
 
-func httpCreateObjectFromHeader(header http.Header) *Object {
-	obj := &Object{Value: map[string]Value{}}
-	for k, vals := range header {
-		obj.Value[k] = &String{Value: vals[0]}
-	}
-	return obj
+type RequestError struct {
+	Kind    string
+	Message string
+	Err     error
+}
+
+func (e *RequestError) Error() string {
+	return fmt.Sprintf("[%s] %s: %v", e.Kind, e.Message, e.Err)
 }
