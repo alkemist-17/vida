ad702971ee9c34a5c140d8a0498485b65bf8059e
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-06-23 12:24:04 -0500

    generate const global errors

--- verror/verror.go
+++ verror/verror.go
@@ -1,7 +1,6 @@
 package verror
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -59,39 +58,45 @@ func (sfi StackFrameInfo) Error() string {
 	return fmt.Sprintf("\tScript    : %v\n\tNear line : %v\n", sfi.ScriptID, sfi.Line)
 }
 
-var (
-	ErrStringLimit                      = errors.New("strings max size has been reached")
-	ErrOpNotDefinedForIterators         = errors.New("operation not defined for iterators")
-	ErrValueNotIndexable                = errors.New("value is not indexable")
-	ErrPrefixOpNotDefined               = errors.New("prefix operation not defined")
-	ErrBinaryOpNotDefined               = errors.New("binary operation not defined")
-	ErrDivisionByZero                   = errors.New("division by zero not defined")
-	ErrExpectedInteger                  = errors.New("expected a value of type integer")
-	ErrExpectedIntegerDifferentFromZero = errors.New("expected an integer value different from zero")
-	ErrValueNotIterable                 = errors.New("value is not iterable")
-	ErrValueNotCallable                 = errors.New("value is not callable")
-	ErrStackOverflow                    = errors.New("stack overflow")
-	ErrArity                            = errors.New("given arguments count is different from arity definition")
-	ErrNotEnoughArgs                    = errors.New("not given enough arguments to the function")
-	ErrVariadicArgs                     = errors.New("expected an array for variradic arguments or array length overflows the current stack")
-	ErrSlice                            = errors.New("could not process the slice")
-	ErrValueIsConstant                  = errors.New("value is constant")
-	ErrMaxMemSize                       = errors.New("max memory size reached")
-	ErrNotImplemented                   = errors.New("not implemented functionality for this value")
-	ErrNotThread                        = errors.New("value is not a thread value")
-	ErrResumingNotSuspendedThread       = errors.New("cannot run a completed, running or waiting thread")
-	ErrNotAFunction                     = errors.New("threads must be build from function values")
-	ErrSuspendingMainThread             = errors.New("cannot suspend the main thread")
-	ErrSuspendingRunningThread          = errors.New("cannot suspend a running thread")
-	ErrClosingAThread                   = errors.New("cannot complete a running, waiting or completed thread")
-	ErrStartThreadSignal                = errors.New("start thread signal")
-	ErrResumeThreadSignal               = errors.New("resume thread signal")
-	ErrSuspendThreadSignal              = errors.New("suspend thread signal")
-	ErrRecyclingThread                  = errors.New("cannot recycle an active thread")
-	ErrSoringMixedTypes                 = errors.New("cannot sort mixed value types")
-	ErrParallelArgs                     = errors.New("arguments for parallel tasks must be non empty arrays")
-	ErrParallelFn                       = errors.New("first argument of a parallel argument must be a function")
-	ErrNonNegativeIntegerTimeout        = errors.New("timeout must be a non negative integer milliseconds")
-	ErrNonEmptyTaskArray                = errors.New("parallel tasks must be inside a non empty array")
-	ErrInvalidJSON                      = errors.New("invalid json")
+type internalBasicError string
+
+func (ibe internalBasicError) Error() string {
+	return string(ibe)
+}
+
+const (
+	ErrStringLimit                      = internalBasicError("strings max size has been reached")
+	ErrOpNotDefinedForIterators         = internalBasicError("operation not defined for iterators")
+	ErrValueNotIndexable                = internalBasicError("value is not indexable")
+	ErrPrefixOpNotDefined               = internalBasicError("prefix operation not defined")
+	ErrBinaryOpNotDefined               = internalBasicError("binary operation not defined")
+	ErrDivisionByZero                   = internalBasicError("division by zero not defined")
+	ErrExpectedInteger                  = internalBasicError("expected a value of type integer")
+	ErrExpectedIntegerDifferentFromZero = internalBasicError("expected an integer value different from zero")
+	ErrValueNotIterable                 = internalBasicError("value is not iterable")
+	ErrValueNotCallable                 = internalBasicError("value is not callable")
+	ErrStackOverflow                    = internalBasicError("stack overflow")
+	ErrArity                            = internalBasicError("given arguments count is different from arity definition")
+	ErrNotEnoughArgs                    = internalBasicError("not given enough arguments to the function")
+	ErrVariadicArgs                     = internalBasicError("expected an array for variradic arguments or array length overflows the current stack")
+	ErrSlice                            = internalBasicError("could not process the slice")
+	ErrValueIsConstant                  = internalBasicError("value is constant")
+	ErrMaxMemSize                       = internalBasicError("max memory size reached")
+	ErrNotImplemented                   = internalBasicError("not implemented functionality for this value")
+	ErrNotThread                        = internalBasicError("value is not a thread value")
+	ErrResumingNotSuspendedThread       = internalBasicError("cannot run a completed, running or waiting thread")
+	ErrNotAFunction                     = internalBasicError("threads must be build from function values")
+	ErrSuspendingMainThread             = internalBasicError("cannot suspend the main thread")
+	ErrSuspendingRunningThread          = internalBasicError("cannot suspend a running thread")
+	ErrClosingAThread                   = internalBasicError("cannot complete a running, waiting or completed thread")
+	ErrStartThreadSignal                = internalBasicError("start thread signal")
+	ErrResumeThreadSignal               = internalBasicError("resume thread signal")
+	ErrSuspendThreadSignal              = internalBasicError("suspend thread signal")
+	ErrRecyclingThread                  = internalBasicError("cannot recycle an active thread")
+	ErrSoringMixedTypes                 = internalBasicError("cannot sort mixed value types")
+	ErrParallelArgs                     = internalBasicError("arguments for parallel tasks must be non empty arrays")
+	ErrParallelFn                       = internalBasicError("first argument of a parallel argument must be a function")
+	ErrNonNegativeIntegerTimeout        = internalBasicError("timeout must be a non negative integer milliseconds")
+	ErrNonEmptyTaskArray                = internalBasicError("parallel tasks must be inside a non empty array")
+	ErrInvalidJSON                      = internalBasicError("invalid json")
 )
