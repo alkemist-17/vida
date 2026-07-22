da3debbe8be465ed74f02ed0a59dc28942d5a823
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2026-07-21 21:50:26 -0500

    add encode decode functions

--- json.go
+++ json.go
@@ -7,9 +7,11 @@ import (
 )
 
 func loadFoundationJSON() Value {
-	m := &Object{Value: make(map[string]Value, 4)}
+	m := &Object{Value: make(map[string]Value, 6)}
 	m.Value["stringify"] = NativeFunction(jsonValueToJsonString)
 	m.Value["parse"] = NativeFunction(jsonParse)
+	m.Value["encode"] = NativeFunction(jsonValueToJsonBytes)
+	m.Value["decode"] = NativeFunction(jsonDecode)
 	m.Value["isValid"] = NativeFunction(jsonIsValid)
 	m.Value["pretty"] = NativeFunction(jsonPretty)
 	return m
@@ -25,6 +27,18 @@ func jsonValueToJsonString(ctx *Context, args ...Value) (Value, error) {
 	}
 	b, _ := json.Marshal(Nil)
 	return &String{Value: string(b)}, nil
+}
+
+func jsonValueToJsonBytes(ctx *Context, args ...Value) (Value, error) {
+	if len(args) > 0 {
+		if b, err := json.Marshal(args[0]); err == nil {
+			return &Bytes{Value: b}, nil
+		} else {
+			return &VidaError{Message: &String{Value: err.Error()}}, nil
+		}
+	}
+	b, _ := json.Marshal(Nil)
+	return &Bytes{Value: b}, nil
 }
 
 func jsonParse(ctx *Context, args ...Value) (Value, error) {
@@ -59,6 +73,43 @@ func jsonParse(ctx *Context, args ...Value) (Value, error) {
 			} else {
 				return &VidaError{Message: &String{Value: err.Error()}}, nil
 			}
+		case *Bytes:
+			var value any
+			if err := json.Unmarshal(t.Value, &value); err == nil {
+				switch v := value.(type) {
+				case nil:
+					return Nil, nil
+				case bool:
+					return Bool(v), nil
+				case string:
+					return &String{Value: v}, nil
+				case float64:
+					return Float(v), nil
+				case map[string]any:
+					return parseObject(ctx, v), nil
+				case []any:
+					return parseArray(ctx, v), nil
+				}
+			} else {
+				return &VidaError{Message: &String{Value: err.Error()}}, nil
+			}
+		}
+	}
+	return Nil, nil
+}
+
+func jsonDecode(ctx *Context, args ...Value) (Value, error) {
+	if len(args) > 0 {
+		valid, _ := jsonIsValid(ctx, args[0])
+		switch t := valid.(type) {
+		case NilValue:
+			return &VidaError{Message: &String{Value: verror.ErrInvalidJSON.Error()}}, nil
+		case Bool:
+			if !t {
+				return &VidaError{Message: &String{Value: verror.ErrInvalidJSON.Error()}}, nil
+			}
+		}
+		switch t := args[0].(type) {
 		case *Bytes:
 			var value any
 			if err := json.Unmarshal(t.Value, &value); err == nil {
